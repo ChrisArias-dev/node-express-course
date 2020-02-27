4458d2714a178b06cab389fbf2b8cfa01cc848ed
Chris Arias-dev <christian.j.arias@gmail.com>
2020-02-27 09:18:20 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function(req, res) {
+  res.json({
+    success: true,
+    message: "successfuly got users. Nice!"
+  });
+});
+
 app.listen(8000, function() {
   console.log("server is running");
 });
